c6e9c3134997bb9790441a8a547f3d7974a3488f
pixaily <danield.georgiev@gmail.com>
2021-09-01 11:00:26 +0300

    Crown Clothing - Add font, styles for menu items and for hover effects

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,16 +4,18 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ size, title, imageUrl, linkUrl }) => {
   return (
-    <div
-      className={size ? 'menu-item ' + size : 'menu-item'}
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}>
+    <div className={size ? 'menu-item ' + size : 'menu-item'}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
-    </div>
+    </div >
   )
 }
 
